9823715e65cb2d19aa896860726b2c4536439af4
Monika <monika@monicode.dev>
2025-06-11 17:48:38 -0600

    Made the 'new' quote subcommand respond with an embed

--- commands/quotes/subcommands/new.js
+++ commands/quotes/subcommands/new.js
@@ -1,18 +1,29 @@
+const { EmbedBuilder } = require("discord.js");
+
+
 module.exports = {
     async execute(interaction) {
         const quotesModel = interaction.client.models.get("quotes")
 
         const quotee = interaction.options.getUser('quotee');
-        const quote_guild = interaction.guildId;
         const quote = interaction.options.getString('quote');
+
+        const botAvatar = interaction.client.user.avatarURL() || interaction.client.user.defaultAvatarURL
 
         quotesModel.create({
             quotee: quotee.id,
-            quote_guild: quote_guild,
+            quote_guild: interaction.guildId,
             quoter: interaction.user.id,
             quote: quote
         })
 
-        await interaction.reply(`Quoting <@${quotee.id}>\n> ${quote}`);
+        const addedNewQuoteEmbed = new EmbedBuilder()
+            .setTitle(`Successfully added new quote!`)
+            .setThumbnail(quotee.avatarURL())
+            .addFields({ name: `New quote for ${quotee.displayName}:`, value: `\`\`\`${quote}\`\`\`` },)
+            .setFooter({ text: `M3`, iconURL: botAvatar })
+            .setTimestamp()
+
+        await interaction.reply({ embeds: [addedNewQuoteEmbed] });
     }
 }
