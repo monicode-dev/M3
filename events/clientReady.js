e2a300cd579c8676f79abb0a662252435dfa76b4
Monika <monika@monicode.dev>
2025-06-14 02:55:05 -0600

    The cannon name of M3 now changes every 5 minutes

--- events/clientReady.js
+++ events/clientReady.js
@@ -5,14 +5,18 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		let multiwords = ["Multipurpose", "Multifuncion", "Multiuse", "Multiversal", "Multidimensional", "Multipcation", "Multitasking", "Multifaceted", "Multilayered", "Multilevel", "Multicolored", "Multispecies", "Multithreaded", "Multiprocessing"]
+
 		client.user.setStatus(PresenceUpdateStatus.Idle);
 		client.statusChangeTimeout = undefined
 
-		client.user.setActivity('Monika\'s Multipurpose Machine', { type: ActivityType.Custom });
-
+		client.user.setActivity(`Monika's ${multiwords[Math.floor(Math.random() * multiwords.length)]} Machine`, { type: ActivityType.Custom });
+		
 		setInterval(function () {
-			log("Task running...");
-		}, 900_000);
+			let newName = `Monika's ${multiwords[Math.floor(Math.random() * multiwords.length)]} Machine`
+			client.user.setActivity(newName, { type: ActivityType.Custom });
+			log.info(`Switching cannon name to "${newName}"`);
+		}, 300_000);
 
 
 		log.info(`Logged in as ${client.user.tag}`)
